3020ebda5667fa062a70cfec6dea36c8d84a8716
osman-beni <osmanbeni2001@gmail.com>
2025-03-08 07:59:41 -0800

    Clean Up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   Link,
   Box,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -24,29 +25,33 @@ function GenreList({ onSelectGenre, genre }: Props) {
   if (error) return null;
 
   return (
-    <List.Root variant="plain">
-      {genres.map((g) => (
-        <List.Item key={g.id} py="5px">
-          <Link
-            onClick={(event) => {
-              event.preventDefault();
-              onSelectGenre(g);
-            }}
-          >
-            <HStack>
-              <Image
-                src={getCroppedImageUrl(g.image_background) || ""}
-                boxSize="32px"
-                borderRadius="md"
-              />
-              <Text fontWeight={genre === g.name ? "bold" : "normal"}>
-                {g.name}
-              </Text>
-            </HStack>
-          </Link>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading mb={3}>Genres</Heading>
+      <List.Root variant="plain">
+        {genres.map((g) => (
+          <List.Item key={g.id} py="5px">
+            <Link
+              onClick={(event) => {
+                event.preventDefault();
+                onSelectGenre(g);
+              }}
+            >
+              <HStack>
+                <Image
+                  src={getCroppedImageUrl(g.image_background) || ""}
+                  boxSize="32px"
+                  borderRadius="md"
+                  objectFit="cover"
+                />
+                <Text fontWeight={genre === g.name ? "bold" : "normal"}>
+                  {g.name}
+                </Text>
+              </HStack>
+            </Link>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 }
 
